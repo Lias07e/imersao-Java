ebf3618ef0f974392ce5d03752a88bced9a95cc3
Elias Ferreira de Melo <elias.ferreira.nego19@gmail.com>
2025-10-27 20:52:53 -0300

    Update MotorTeste.java

--- MeUp1/MotorTeste.java
+++ MeUp1/MotorTeste.java
@@ -3,29 +3,27 @@ package MeUp1;
 public class MotorTeste {
     public static void main (String[]args){
 
-        Motor mt = new Motor("Honda" , 650, "Gasolina");
+         //Motor mt = new Motor("Honda" , 650, "Gasolina");
+
+        Motor [] mt = new Motor[2];
+        mt[0] = new Motor("Honda" , 650 , "Gasolina");
+        mt[1] = new Motor("Twiter" , 650 , "Gasolina");
 
 
-        System.out.println("============ TESTE DA CLASSE MOTOR ================");
-        System.out.println(mt);
 
-        System.out.println("============================");
-        mt.ligarMotor();
-        mt.ligarMotor();
+        System.out.print(mt[0].desligarMotor());
+        System.out.print(mt[0].desligarMotor());
 
-         System.out.println("============================");
-        mt.ligarMotor();
-        mt.desligarMotor();
-        
-        System.out.println("============================");
-        mt.desligarMotor();
-        mt.desligarMotor();
-        
-        System.out.println(mt);
+        System.out.println(mt[1].ligarMotor());
+        System.out.println(mt[1].desligarMotor());
 
 
+
+        System.out.println(mt[0].toString());
+        System.out.println(mt[1].toString());
 
 
     }
 }
+
 
